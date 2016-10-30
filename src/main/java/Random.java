0d30710a35ac5436e49d12185a866a4369ad3a17
incloser <nickmsk@outlook.com>
2016-10-30 13:02:28 +0300

    add new function random

---
+++ src/main/java/Random.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Incloser on 30.10.2016.
+ */
+public class Random {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Math.random());
+        }
+    }
+
+}
