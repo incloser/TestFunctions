7284d3688bebe75229950b9175f1227d3c45454d
incloser <nickmsk@outlook.com>
2016-11-04 23:01:39 +0300

    Добавление selenum в проект

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -32,7 +32,7 @@ public class Test1{
 
     @Test
     public void test() {
-        System.out.println("Второй тест, не далающий ничего.");
+        System.out.println("Второй тест, не делающий ничего.");
     }
 
     @After
