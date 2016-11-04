62f7d03a797a98aae589773c28c9913c8650f122
incloser <nickmsk@outlook.com>
2016-11-04 20:39:31 +0300

    Добавление различных аннотаций junit, изменена версия на последнюю.

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,13 +1,47 @@
-import org.junit.Test;
+import org.junit.*;
 import package1.New;
 
 import static org.junit.Assert.assertEquals;
 
-public class Test1 {
+public class Test1{
+    @BeforeClass
+    public static void mainSetUp() {
+        System.out.println("Начало всех тестов.");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("Начало теста.");
+    }
+
+    @Test(timeout = 1000)
+    public void testRandomConstructor() {
+        New test = new New();
+        try {
+            assertEquals(3, test.add(1, 2));
+            System.out.println("Сложение работает верно.");
+        } catch (Exception e) {
+            System.out.println("Сложение не верно.");
+        }
+    }
+
+    @Ignore("Тест на стадии разработки.")
+    public void ignoreTest() {
+        System.out.println("Этого никто не увидит в тестах.");
+    }
 
     @Test
-    public void testRandomConstructor() throws Exception {
-        New test = new New();
-        assertEquals(3, test.add(1, 2));
+    public void test() {
+        System.out.println("Второй тест, не далающий ничего.");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Конец теста.");
+    }
+
+    @AfterClass
+    public static void mainAfter() {
+        System.out.println("Конец всех тестов.");
     }
 }
