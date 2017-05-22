d0ae277a0ec37b8764dfc241a7acf209debbdb20
wildcard <nickmsk@outlook.com>
2017-05-22 20:38:14 +0300

    Bubble sorting

--- src/main/java/tests/other/VersatileTest.java
+++ src/main/java/tests/other/VersatileTest.java
@@ -1,15 +1,36 @@
 package tests.other;
 
-/**
- * Created by wildcard on 18.05.2017.
- */
-public class VersatileTest {
+import java.util.Arrays;
 
-    static int b = 5;
-    static int a = 0;
+public class VersatileTest {
+    //Bubble sorting
+    private static int[] arr = {5, 3, 9, 1};
 
     public static void main(String[] args) {
-        a = a/b;
-        System.out.println("b: " + b + "a: " + a);
+
+        System.out.println(Arrays.toString(arr));
+        while (!isArrSorted(arr)) {
+            for(int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    interchange(arr, i, i + 1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static boolean isArrSorted(int[] arr) {
+        for(int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void interchange(int[] arr, int curr, int next) {
+        int temp =  arr[curr];
+        arr[curr] = arr[next];
+        arr[next] = temp;
     }
 }
